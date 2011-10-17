d9660b972ca0c7e2c8525e0956c80b2c152d7242
Giovanni <Giovanni@10.12.22.55>
2011-10-17 15:57:55 -0400

    CSS minified

--- New/enhanced_800_plus.php
+++ New/enhanced_800_plus.php
@@ -2,17 +2,4 @@
 header("Content-Type: text/css; charset=utf-8");
 header("Cache-Control: max-age=31536000, public");
 ?>
-@CHARSET "UTF-8";
-
-#content1{
-	width:65%;
-	float:right;
-	border-bottom:1px solid black;	
-}
-
-#content2{
-	width:30%;
-	float:left;
-	font-size: 0.9em;
-	border-top:1px solid black;
-}
+@CHARSET "UTF-8";#content1{	width:65%;float:right;border-bottom:1px solid black;}#content2{width:30%;float:left;font-size: 0.9em;border-top:1px solid black;}
